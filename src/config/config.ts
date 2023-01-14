7214fcc6a6bd233914de9f284c2522c65be08ebc
topahadzi <topahadzi@gmail.com>
2023-01-14 18:12:52 +0700

    init

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,8 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
 export default {
-    port: process.env.PORT || 3000,
+    port: process.env.PORT,
     jwtSecret: process.env.JWT_SECRET || 'somesecrettoken',
     DB: {
       URI: process.env.MONGODB_URI || 'mongodb+srv://epsi:QzzZ6WsBLSqHLyjX@cluster0.xsaxvmh.mongodb.net/?retryWrites=true&w=majority',
