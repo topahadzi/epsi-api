016c0458a2cfbd3e7ede048223c0a3f94bc8f58f
topahadzi <topahadzi@gmail.com>
2023-02-02 20:13:39 +0700

    change expire token to 1y

--- src/validators/generateToken.ts
+++ src/validators/generateToken.ts
@@ -4,7 +4,7 @@ import config from '../config/config'
 export default{
     generateJwt(params = {}) {
         return jwt.sign(params, config.jwtSecret, {
-            expiresIn: 86400
+            expiresIn: '365d'
         })
     }
 }
