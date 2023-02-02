8566bebd8c7a84067bfb7c9ba77db04c56657165
topahadzi <topahadzi@gmail.com>
2023-02-02 23:38:48 +0700

    add alll this night

---
+++ .build/src/models/Berita.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = require("mongoose");
+const BeritaSchema = new mongoose_1.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+    }
+});
+const Berita = (0, mongoose_1.model)("Berita", BeritaSchema);
+exports.default = Berita;
+//# sourceMappingURL=Berita.js.map
